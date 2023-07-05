b415eb2b3c041d1bee62947c2d339a5254252cbf
Vaibhav <vaibhavchauhan2410@gmail.com>
2023-07-05 23:06:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PathfindingVisualizer from './PathFindingVisualizer/PathFindingVisualizer';
+
+function App()
+{
+  return (
+    <div className="App">
+      <PathfindingVisualizer> </PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
